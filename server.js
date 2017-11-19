25e8377e7e4200263d4c3f42306a46b4d274a942
Ricardo <ricardoplouis@gmail.com>
2017-11-19 17:23:31 -0600

    Renaming rooms to chains

--- server.js
+++ server.js
@@ -9,38 +9,38 @@ const nextApp = next({ dev })
 const nextHandler = nextApp.getRequestHandler()
 
 // fake DB
-var rooms = []
+var chains = []
 
 // socket.io server
 io.on('connection', socket => {
-  socket.on('makeRoom', (data) => {
-    rooms.push(data)
-    socket.broadcast.emit('makeRoom', data)
+  socket.on('makeChain', (data) => {
+    chains.push(data)
+    socket.broadcast.emit('makeChain', data)
   })
 
-  socket.on('updateRoom', (data) => {
-    for (let x = rooms.length -1; x >-1 ; x-- ){
-      if (rooms[x].id == data.id){
-        rooms[x] = data;
+  socket.on('updateChain', (data) => {
+    for (let x = chains.length -1; x >-1 ; x-- ){
+      if (chains[x].id == data.id){
+        chains[x] = data;
       }
     }
-    socket.broadcast.emit('updateRoom', data);
+    socket.broadcast.emit('updateChain', data);
   })
 })
 
 nextApp.prepare().then(() => {
-  app.get('/rooms', (req, res) => {
+  app.get('/chains', (req, res) => {
     const queryParams = { id: req.query.id}
     if (req.query.id){
-      nextApp.render(req, res, '/rooms' , queryParams)
+      nextApp.render(req, res, '/chains' , queryParams)
     } else {
-      res.json(rooms)
+      res.json(chains)
     }
   })
 
-  app.get('/rooms:id', (req, res) => {
+  app.get('/chains:id', (req, res) => {
     const queryParams = { id: req.query.id}
-    nextApp.render(req, res, '/rooms' ,  queryParams)
+    nextApp.render(req, res, '/chains' ,  queryParams)
   })
 
   app.get('*', (req, res) => {
